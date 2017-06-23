2b13fdb696a0dde17c98bd0f0542e5f910a9dbc8
User <paadams@nmu.edu>
2017-06-23 08:02:57 -0400

    added tests for appstate

--- src/tests/CartStore.test.js
+++ src/tests/CartStore.test.js
@@ -2,12 +2,6 @@ import Store from '../stores/store'
 
 const store = new Store()
 const cartStore = store.cartStore
-const inventoryStore = store.inventoryStore
-
 it('items contains 1 item', () => {
     expect(cartStore.getItemsInCart.length).toEqual(1)
-})
-
-it('add item to inventory should increase inventory by 1', () => {
-    
 })
---
+++ src/tests/Inventory.test.js
@@ -0,0 +1,14 @@
+import Store from '../stores/store'
+
+const store = new Store()
+const inventoryStore = store.inventoryStore
+
+it('add item to inventory should increase inventory by 1', () => {
+    inventoryStore.addNewItem("WKND Molinar", 2, 50.00)
+    expect(inventoryStore.inventory.length).toEqual(2)
+})
+
+it('third item in inventory should have id of 3', () => {
+    inventoryStore.addNewItem("X BOX ONE", 5, 250.00)
+    expect(inventoryStore.inventory[2].id).toEqual(3)
+})
