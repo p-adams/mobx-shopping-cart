23b635efb88cbb14f7b41d70b0c43105dcca02e1
User <paadams@nmu.edu>
2017-06-25 00:55:20 -0400

    updated CartDisplay component

--- src/CartDisplay.js
+++ src/CartDisplay.js
@@ -1,17 +1,26 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {inject, observer} from 'mobx-react'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
+import {Card, CardHeader, CardText} from 'material-ui/Card'
 
 const CartDisplay = inject("cart")(observer (({cart}) => {
-    
+        const itemStyle = {
+            background: "white",
+            border: "1px solid red",
+            margin: '10px'
+        }
         return (
             <Drawer
                 open={cart.isCartOpen}
                 openSecondary={true}>
                 <MenuItem>
                     {cart.getItemsInCart.map((item, index) => {
-                        return <div key={index}>{item.item}</div>
+                        return (
+                            <Card style={itemStyle} key={index}>
+                                <CardHeader titleColor="black" title={item.item}/>
+                            </Card>
+                        )
                     })}
                  </MenuItem>
             </Drawer>
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -5,7 +5,8 @@ class CartStore {
         extendObservable(this, {
             isCartOpen: false,
             itemsInCart: [
-                {id: 1, item: "REAL ISHOD WAIR", quantity: 1, price: 49.00}
+                {id: 1, item: "REAL ISHOD WAIR", quantity: 1, price: 49.00},
+                {id: 2, item: 'PLAN B JOSLIN', quantity: 1, price: 52.00}
             ],
             getItemsInCart: computed(() => {
                 return this.itemsInCart
